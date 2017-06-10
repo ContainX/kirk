1c08cf3c0793f82d7a34295e2ecaae73723f7524
Jeremy Roberts <jrobert@ten-x.com>
2017-06-10 16:40:40 -0700

    Adding real JIRA links

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"fmt"
 	"strings"
-	"unicode"
+	"regexp"
 )
 
 func main() {
@@ -14,43 +14,39 @@ func main() {
 		"SLACK_BOT_ACCESS_TOKEN": "xoxb-196008087415-z3m59xI3h3DMMZtLovYsZY40",
 	}
 
-	//jiraUrlBase := "https://jira.auction.com/browse/"
-	jiraProjectKey := "XOEY"
-
-
-
 	api := slack.New(config["SLACK_BOT_ACCESS_TOKEN"])
 	logger := log.New(os.Stdout, "slack-bot:", log.Lshortfile|log.LstdFlags)
 
 	slack.SetLogger(logger)
-	api.SetDebug(true)
+	//api.SetDebug(true)
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
+	const connectionChannel string = "C5QF2Q4NL"
+	const jiraBaseUrl string = "https://jira.auction.com"
+	projects := []string{"VOLT", "XOEY"}
 
 	for msg := range rtm.IncomingEvents {
-		fmt.Print("Event Received")
+		fmt.Println("Event Received")
 
 		switch event := msg.Data.(type) {
-		case *slack.ConnectedEvent:
-			fmt.Println("Infos:", event.Info)
-			fmt.Println("Connection counter:", event.ConnectionCount)
-			// Replace #general with your Channel ID
-			rtm.SendMessage(rtm.NewOutgoingMessage("This is. your captain.  I. am. here.", "C5QF2Q4NL"))
+		//case *slack.ConnectedEvent:
+		//	fmt.Println("Kirk is connected to Slack")
+		//	fmt.Println("Connection counter:", event.ConnectionCount)
 		case *slack.MessageEvent:
 			text := event.Text
-			if strings.Contains(text, jiraProjectKey) {
-				// TODO: Transformation not working
-				indexOfProjectKey := strings.Index(text, jiraProjectKey)
-				indexOfCharAfterIssueKey := strings.IndexFunc(
-					text[indexOfProjectKey:len(text)-1],
-					func(c rune) bool {
-						return !unicode.IsDigit(c)
-					},
-				)
-				jiraIssueId := text[indexOfProjectKey:indexOfCharAfterIssueKey]
-				fmt.Println(text, indexOfProjectKey, indexOfCharAfterIssueKey)
-				fmt.Printf("JIRA ISSUE FOUND: %v\n", jiraIssueId)
+
+			issueIdRegex := "(" + strings.Join(projects, "|") + `)-\d+`
+			re := regexp.MustCompile(issueIdRegex)
+			issueIds := re.FindAllString(text, -1)
+
+			if issueIds != nil {
+				newText := "Here are. some. links to JIRA:\n"
+				for _, issueId := range issueIds {
+					newText += jiraBaseUrl + "/browse/" + issueId + "\n"
+				}
+
+				rtm.SendMessage(rtm.NewOutgoingMessage(newText, connectionChannel))
 			}
 		}
 	}
