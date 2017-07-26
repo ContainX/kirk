c8aca8d2b06f668f033a83f2678694adea08bed3
Jeremy Roberts <jeremyroberts0@gmail.com>
2017-07-25 22:08:07 -0700

    #2 - Added simple kirkbot homepage

--- router.go
+++ router.go
@@ -10,6 +10,8 @@ func getRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.LoadHTMLGlob("html/*")
+	router.StaticFile("/", "./static/html/index.html")
+	router.Static("/assets", "./static/assets")
 	routes.Info(router)
 	routes.Auth(router)
 
